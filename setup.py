eb5bbfbbcd5aef4c4cee9f696140263a6999f370
Lennard de Vries <lennard@fourdigits.nl>
2020-12-08 13:09:41 +0100

    release overmind 0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Marko Samastur",
     author_email="markos@gaivo.net",
     name='image-diet',
-    version='0.7.1',
+    version='0.1.0-fourdigits-61d6c085',
     description='Remove unnecessary bytes from images',
     long_description=long_description,
     url='https://github.com/samastur/image-diet/',
